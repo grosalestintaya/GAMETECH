deedb1b9209be7ae8ea4bf65bd3f4d5820ba2142
George  Styff Rosales Tintaya <63511895+grosalestintaya@users.noreply.github.com>
2024-08-26 19:42:07 -0500

    Update config.php

--- GamerTech/includes/config.php
+++ GamerTech/includes/config.php
@@ -1,7 +1,7 @@
 <?php
-define('DB_SERVER','sql102.infinityfree.com');
-define('DB_USER','if0_37178674');
-define('DB_PASS' ,'maxraulcarrasco');
+define('DB_SERVER','finalperro.cjio0iamoi8b.us-east-1.rds.amazonaws.com:8888');
+define('DB_USER','admin');
+define('DB_PASS' ,'76668813');
 define('DB_NAME', 'if0_37178674_shopping');
 $con = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
 // Check connection
