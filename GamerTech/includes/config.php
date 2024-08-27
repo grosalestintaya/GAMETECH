e946532775f4fe323ff3769b3abecf348ecd17ed
George  Styff Rosales Tintaya <63511895+grosalestintaya@users.noreply.github.com>
2024-08-26 21:52:47 -0500

    Update config.php

--- GamerTech/includes/config.php
+++ GamerTech/includes/config.php
@@ -1,5 +1,5 @@
 <?php
-define('raaaaa.cjio0iamoi8b.us-east-1.rds.amazonaws.com:3306');
+define('DB_SERVER','raaaaa.cjio0iamoi8b.us-east-1.rds.amazonaws.com:3306');
 define('DB_USER','admin');
 define('DB_PASS' ,'76668813');
 define('DB_NAME', 'centro_psicologo');
